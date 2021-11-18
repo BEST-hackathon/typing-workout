a4eb5c04a2227abd0a775bee55710df1cbde4fd9
nick <nikyasnogorodskyi@gmail.com>
2021-11-18 23:02:05 +0200

    feat: move time settings, make type-area larger, fix context

---
+++ components/AttemptDurationSettings.tsx
@@ -0,0 +1,25 @@
+import { useTypingCtx } from '../context/TypingContext'
+import cs from 'classnames'
+import styles from '../styles/Typing.module.css'
+
+const availableTimes = [15, 30, 45]
+
+export const AttemptDurationSettings = () => {
+    const { attemptDuration, setAttemptDuration } = useTypingCtx()
+
+    return (
+        <div className={styles.timePickBox}>
+            {availableTimes.map((time, idx) => (
+                <div
+                    key={idx}
+                    className={cs(styles.timePick, {
+                        [styles.active]: time === attemptDuration,
+                    })}
+                    onClick={() => setAttemptDuration(time)}
+                >
+                    {time}
+                </div>
+            ))}
+        </div>
+    )
+}
--- components/TypingContainer.tsx
+++ components/TypingContainer.tsx
@@ -4,30 +4,12 @@ import styles from '../styles/Typing.module.css'
 import { useTypingCtx } from '../context/TypingContext'
 
 export const TypingContainer = () => {
-    const { restart, secondsLeft, setAttemptDuration } = useTypingCtx()
-
-    const availableTimes = [15, 30, 45]
+    const { restart, secondsLeft } = useTypingCtx()
 
     return (
         <div className={styles.typingContainer}>
-            <div className={styles.time}>
-                <p>
-                    {secondsLeft} (
-                    {availableTimes.map((time, index, array) => (
-                        <>
-                            <a
-                                key={index}
-                                className={styles.timePick}
-                                onClick={() => setAttemptDuration(time)}
-                            >
-                                {time}
-                            </a>
-                            {array.length - 1 !== index && ' | '}
-                        </>
-                    ))}
-                    )
-                </p>
-            </div>
+            <div className={styles.time}>{secondsLeft}</div>
+
             <TypingBox />
 
             <div className={styles.restart} onClick={restart}>
